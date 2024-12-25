3a0972311f351dd787a488aeb17c5d67be9f03c9
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2024-12-26 01:03:45 +0500

    Header

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header className="flex bg-border border border-[#262626] px-5 md:px-12 justify-between items-center p-3 md:p-6">
+    <header className="flex bg-border border border-[#262626] px-5 md:px-12 justify-between items-center p-3 md:p-5">
         <Link href={"/"} ><Image className="w-1/2 sm:w-4/5" src={icon} alt="Blog Site"/></Link>
         <nav>
             <ul className="flex gap-3 md:gap-6 text-sm md:text-lg">
