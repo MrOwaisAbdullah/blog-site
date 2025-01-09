c101bbb955ee499c59bbace324b97f5a23250eef
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2025-01-09 18:33:25 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Hero from "@/components/sections/Hero";
 export default async function Home() {
 
   return (
-    <div className="max-w-[1600px] flex flex-col items-center min-h-screen mb-20">
+    <div className="max-w-[1600px] flex flex-col items-center min-h-screen mx-auto mb-20">
       <Hero />
       <div className="border-b-4 px-5 sm:px-10 border-border my-6">
       <h2 className={`${kumbhSans.className} tracking-widest font-bold text-3xl xl:text-5xl 2xl:text-6xl text-heading text-center uppercase py-5 mt-5`}>Latest Blog</h2>
