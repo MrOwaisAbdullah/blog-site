3a9b29cbb5722fa341af689e121a9851966cc5a0
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2025-01-09 16:55:23 +0500

    updated

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,11 +1,11 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, token } from '../env'
+import { apiVersion, dataset, projectId, token } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
+  useCdn: false,
   token,
-})
+});
